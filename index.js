b932aaf4483f896b4f5b6df008892643a3002bec
Enane12 <madyange@yahoo.com>
2024-12-08 13:14:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Navbar toggle for mobile view
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
